c0e42c5e25fedbb742f82680fd45e5f9bb0945a3
sskkilm <sskkilm@naver.com>
2024-05-01 20:55:21 +0900

    test : 로그인 테스트 코드 구현

--- src/test/java/com/sskkilm/cashflow/controller/UserControllerTest.java
+++ src/test/java/com/sskkilm/cashflow/controller/UserControllerTest.java
@@ -1,10 +1,14 @@
 package com.sskkilm.cashflow.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sskkilm.cashflow.config.AppConfiguration;
 import com.sskkilm.cashflow.config.SecurityConfiguration;
 import com.sskkilm.cashflow.dto.JoinDto;
+import com.sskkilm.cashflow.entity.User;
+import com.sskkilm.cashflow.enums.Authority;
 import com.sskkilm.cashflow.exception.CustomException;
 import com.sskkilm.cashflow.service.UserService;
+import com.sskkilm.cashflow.util.JwtUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +16,37 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.sskkilm.cashflow.enums.UserErrorCode.ALREADY_EXIST_USER;
+import static com.sskkilm.cashflow.enums.UserErrorCode.LOGIN_FAILED;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-@Import(SecurityConfiguration.class)
+@Import({SecurityConfiguration.class, AppConfiguration.class})
 class UserControllerTest {
 
     @MockBean
     private UserService userService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
     @DisplayName("회원가입 성공")
@@ -45,12 +58,12 @@ class UserControllerTest {
                                 .id(1L)
                                 .loginId("root")
                                 .password("root")
-                                .role("USER")
+                                .role(Authority.ROLE_USER)
                                 .build()
                 );
         //when
         //then
-        mockMvc.perform(post("/join")
+        mockMvc.perform(post("/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(
                                 new JoinDto.Request("root", "root")
@@ -59,7 +72,7 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.id").value(1L))
                 .andExpect(jsonPath("$.loginId").value("root"))
                 .andExpect(jsonPath("$.password").value("root"))
-                .andExpect(jsonPath("$.role").value("USER"))
+                .andExpect(jsonPath("$.role").value("ROLE_USER"))
                 .andDo(print());
     }
 
@@ -71,7 +84,7 @@ class UserControllerTest {
                 .willThrow(new CustomException(ALREADY_EXIST_USER));
         //when
         //then
-        mockMvc.perform(post("/join")
+        mockMvc.perform(post("/users/join")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(
                                 new JoinDto.Request("root", "root")
@@ -80,6 +93,50 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.status").value(400))
                 .andExpect(jsonPath("$.errorCode").value("ALREADY_EXIST_USER"))
                 .andExpect(jsonPath("$.message").value(ALREADY_EXIST_USER.getMessage()))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    void login_success() throws Exception {
+        //given
+        given(userService.loadUserByUsername(any()))
+                .willReturn(User.builder()
+                        .loginId("root")
+                        .password(bCryptPasswordEncoder.encode("root"))
+                        .role(Authority.ROLE_USER)
+                        .build()
+                );
+        given(jwtUtil.createJwt(any(), any(), anyLong()))
+                .willReturn("TEST_TOKEN");
+
+        //when
+        //then
+        mockMvc.perform(post("/users/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("loginId", "root")
+                        .param("password", "root"))
+                .andExpect(header().string("Authorization", "Bearer TEST_TOKEN"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 존재하지 않는 사용자")
+    void login_fail() throws Exception {
+        //given
+        given(userService.loadUserByUsername(any()))
+                .willThrow(new UsernameNotFoundException("user not found"));
+
+        //when
+        //then
+        mockMvc.perform(post("/users/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("loginId", "root")
+                        .param("password", "root"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.errorCode").value("LOGIN_FAILED"))
+                .andExpect(jsonPath("$.message").value(LOGIN_FAILED.getMessage()))
                 .andDo(print());
     }
 }
--- src/test/java/com/sskkilm/cashflow/service/UserServiceTest.java
+++ src/test/java/com/sskkilm/cashflow/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.sskkilm.cashflow.service;
 
 import com.sskkilm.cashflow.dto.JoinDto;
 import com.sskkilm.cashflow.entity.User;
+import com.sskkilm.cashflow.enums.Authority;
 import com.sskkilm.cashflow.enums.UserErrorCode;
 import com.sskkilm.cashflow.exception.CustomException;
 import com.sskkilm.cashflow.repository.UserRepository;
@@ -11,7 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -40,7 +44,7 @@ class UserServiceTest {
                 .willReturn(User.builder()
                         .loginId("root")
                         .password("root")
-                        .role("USER")
+                        .role(Authority.ROLE_USER)
                         .build());
         given(bCryptPasswordEncoder.encode(any()))
                 .willReturn("root");
@@ -53,7 +57,7 @@ class UserServiceTest {
         //then
         assertEquals("root", response.loginId());
         assertEquals("root", response.password());
-        assertEquals("USER", response.role());
+        assertEquals(Authority.ROLE_USER, response.role());
     }
 
     @Test
@@ -74,4 +78,38 @@ class UserServiceTest {
         assertEquals(UserErrorCode.ALREADY_EXIST_USER, customException.getErrorCode());
     }
 
+    @Test
+    @DisplayName("유저 정보 불러오기 성공")
+    void loadUserByUsername_success() {
+        //given
+        given(userRepository.findByLoginId(any()))
+                .willReturn(Optional.of(User.builder()
+                        .loginId("root")
+                        .password("root")
+                        .role(Authority.ROLE_USER)
+                        .build()));
+        //when
+        UserDetails userDetails = userService.loadUserByUsername("root");
+
+        //then
+        assertEquals("root", userDetails.getUsername());
+        assertEquals("root", userDetails.getPassword());
+        assertEquals("ROLE_USER", userDetails.getAuthorities().iterator().next().getAuthority());
+    }
+
+    @Test
+    @DisplayName("유저 정보 불러오기 실패 - 존재하지 않는 사용자")
+    void loadUserByUsername_fail() {
+        //given
+        given(userRepository.findByLoginId(any()))
+                .willReturn(Optional.empty());
+        //when
+        CustomException customException = assertThrows(CustomException.class,
+                () -> userService.loadUserByUsername("root"));
+
+        //then
+        assertEquals(400, customException.getErrorCode().getStatus());
+        assertEquals(UserErrorCode.USER_NOT_FOUND, customException.getErrorCode());
+        assertEquals("존재하지 않는 사용자입니다.", customException.getErrorCode().getMessage());
+    }
 }
